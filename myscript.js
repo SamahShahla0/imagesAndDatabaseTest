bdd0b8f3351dd7b9c2b9974dc13fcfcc9ad2b00f
SamahShahla0 <samahch6@gmail.com>
2024-02-15 16:44:01 +0200

    --

--- myscript.js
+++ myscript.js
@@ -1,21 +1,21 @@
 // Fetch items from the server
 fetch("get_items.php")
 .then((response) => response.json())
-.then((items) => {
+.then((products) => {
   const itemsContainer = document.getElementById("itemsContainer");
-  items.forEach((item) => {
+  products.forEach((product) => {
     const itemElement = document.createElement("div");
     itemElement.classList.add("item");
 
     const imgElement = document.createElement("img");
-    imgElement.src = "data:image/jpeg;base64," + item.image_base64;
-    imgElement.alt = item.name;
+    imgElement.src = "data:image/jpeg;base64," + product.image_base64;
+    imgElement.alt = product.name;
 
     const h2Element = document.createElement("h2");
-    h2Element.textContent = item.name;
+    h2Element.textContent = product.name;
 
     const pElement = document.createElement("p");
-    pElement.textContent = "Description of " + item.name;
+    pElement.textContent = "Description of " + product.name;
 
     itemElement.appendChild(imgElement);
     itemElement.appendChild(h2Element);
